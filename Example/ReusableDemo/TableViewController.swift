659b3fa0b756187e5c67ec174af1c07c98540173
Jakub Skotnicki <jakubskot@gmail.com>
2017-02-16 00:18:05 +0100

    overriding init(frame:) of MyHeaderTableView to automatically load the view content from its nib using loadNibContent()

--- Example/ReusableDemo/TableViewController.swift
+++ Example/ReusableDemo/TableViewController.swift
@@ -27,8 +27,12 @@ final class TableViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let view = MyHeaderTableView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: self.tableView(tableView, heightForHeaderInSection: section)))
-    view.loadNibContent()
+    let frame = CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: self.tableView(tableView, heightForHeaderInSection: section))
+    
+    // See the overridden `MyHeaderTableView.init(frame:)` initializer, which
+    // automatically loads the view content from its nib using loadNibContent()
+    let view = MyHeaderTableView(frame: frame)
+    
     view.fillForSection(section)
     return view
   }
