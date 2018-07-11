98453b66eeb599a9d298bfdaf7ac74a5c643216b
Olivier Halligon <olivier@halligon.net>
2018-07-11 14:05:28 +0200

    Fix SwiftLint violations.

--- Example/ReusableDemo tvOS/AppDelegate.swift
+++ Example/ReusableDemo tvOS/AppDelegate.swift
@@ -13,11 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
   }
-
 }
 
 /// Swift < 4.2 support
