9a495ad251d221d68ce712841b06b380f332de9e
sebcoh <35079848+sebcoh@users.noreply.github.com>
2021-09-07 09:01:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "Reusable",
-    platforms: [.iOS(.v8), .tvOS(.v9)],
     products: [
         .library(name: "Reusable", targets: ["Reusable"])
     ],
